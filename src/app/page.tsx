befd92b24b73163b06a5eaaaf4810c903420feb5
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-05-04 10:57:07 +0530

    Added color wheel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,43 @@
 "use client";
 import useDraw from "@/hooks/useDraw";
-import drawLine from "@/utilities/drawLine";
-import React, { useRef } from "react";
-
+import React, { useRef, useState } from "react";
+import {Wheel } from '@uiw/react-color';
+import { EditableInput, EditableInputRGBA, EditableInputHSLA } from '@uiw/react-color';
 const page = () => {
   const { canvasRef, handler,onClearCanvas} = useDraw(drawLine);
-  
+  const [hex, setHex] = useState("#fff");
+
+
+  function drawLine({ ctx, prevPoint, currentPoint }: Draw) {
+    if (prevPoint) {
+      ctx!.beginPath();
+      ctx!.strokeStyle = hex;
+      ctx?.moveTo(prevPoint.x, prevPoint.y);
+      ctx?.lineTo(currentPoint.x, currentPoint.y);
+      ctx?.stroke();
+    }
+  }
   return (
     <div className="flex flex-row justify-center items-center h-[600px]">
-      <div className="flex flex-col justify-center items-center mx-4 w-[100px] ">
-        <button type="button" onClick={onClearCanvas} className="bg-white text-black bg-round-full">
+      <div className="flex flex-col justify-center items-center mx-4 w-[300px] h-[600px]">
+        <Wheel
+          style={{ marginLeft: 20 }}
+          color={hex}
+          onChange={(color) => {
+            setHex(color.hex);
+          }}
+        />
+        <button
+          type="button"
+          onClick={onClearCanvas}
+          className="bg-white text-black rounded-full px-4 py-2 my-3 shadow-md hover:bg-gray-200 focus:outline-none focus:ring focus:ring-gray-400"
+        >
           Clear Canvas
         </button>
       </div>
       <canvas
         ref={canvasRef}
-        width={700}
+        width={1000}
         height={500}
         className="bg-white border border-3 border-solid border-gray rounded-lg"
         onMouseMove={handler}
--- src/utilities/drawLine.tsx
+++
@@ -1,9 +0,0 @@
-export default function drawLine({ ctx, prevPoint, currentPoint }: Draw) {
-    if (prevPoint) {
-      ctx!.beginPath();
-      ctx!.strokeStyle = "black";
-      ctx?.moveTo(prevPoint.x, prevPoint.y);
-      ctx?.lineTo(currentPoint.x, currentPoint.y);
-      ctx?.stroke();
-    }
-  }
